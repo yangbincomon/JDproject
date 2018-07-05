6f8dbc908df18ad1726ff69c4afb12e1dd5c17b7
b.yang <760350398@qq.com>
2018-07-05 19:36:23 +0800

    YB 基础框架 1.1

---
+++ JD-dubbo-zookeeper-customer/src/main/java/com/jk/model/znn/Test.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2018, bin.yang.com
+ * FileName: Test
+ * Author:   杨斌
+ * Date:     2018/7/5 0005 19:31
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.model.znn;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 杨斌
+ * @create 2018/7/5 0005
+ * @since 1.0.0
+ */
+
+public class Test {
+}
