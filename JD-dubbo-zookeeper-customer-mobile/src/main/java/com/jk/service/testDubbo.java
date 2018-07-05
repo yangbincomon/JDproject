b46c1e34f401b1929b239897dec6a3d0441ef193
b.yang <760350398@qq.com>
2018-07-05 14:21:15 +0800

    Initial commit

---
+++ JD-dubbo-zookeeper-customer-mobile/src/main/java/com/jk/service/testDubbo.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2018, bin.yang.com
+ * FileName: testDubbo
+ * Author:   杨斌
+ * Date:     2018/7/4 0004 17:01
+ * Description:
+ * History:
+ * <author>            <time>              <version>            <desc>
+ * 作者姓名:           修改时间:           版本号:              描述:
+ */
+package com.jk.service;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 杨斌
+ * @create 2018/7/4 0004
+ * @since 1.0.0
+ */
+
+public interface testDubbo {
+      void  tests();
+}
---
+++ JD-dubbo-zookeeper-provider-product/src/main/java/com/jk/service/impl/testDubboImpl.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C), 2015-2018, bin.yang.com
+ * FileName: testDubboImpl
+ * Author:   杨斌
+ * Date:     2018/7/4 0004 17:02
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.service.impl;
+
+import com.jk.service.testDubbo;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 杨斌
+ * @create 2018/7/4 0004
+ * @since 1.0.0
+ */
+
+public class testDubboImpl implements testDubbo {
+
+    public void tests() {
+        System.out.println("success");
+    }
+}
